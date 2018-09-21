f9892c37b074f669467d5ea1205950edf4e003e5
Mavolas <sgngqian@sina.com>
2018-09-21 19:31:32 +0800

    完善

---
+++ mavo-core/src/main/java/com/mavolas/mavo/activitys/ProxyActivity.java
@@ -0,0 +1,10 @@
+package com.mavolas.mavo.activitys;
+
+import me.yokeyword.fragmentation.SupportActivity;
+
+/**
+ * Created by 宋棋安
+ * on 2018/9/21.
+ */
+public abstract class ProxyActivity extends SupportActivity {
+}
---
+++ mavo-core/src/main/java/com/mavolas/mavo/delegates/MavoDelegate.java
@@ -0,0 +1,8 @@
+package com.mavolas.mavo.delegates;
+
+/**
+ * Created by 宋棋安
+ * on 2018/9/21.
+ */
+public class MavoDelegate  {
+}
