298e57f35011c99abff08ee1951880beee7ee4bf
Mavolas <sgngqian@sina.com>
2018-09-26 18:59:31 +0800

    网络框基上传完成

--- mavo-core/src/main/java/com/mavolas/mavo/net/RestClient.java
+++ mavo-core/src/main/java/com/mavolas/mavo/net/RestClient.java
@@ -10,9 +10,12 @@ import com.mavolas.mavo.net.callback.RequestCallbacks;
 import com.mavolas.mavo.ui.LoaderStyle;
 import com.mavolas.mavo.ui.MavoLoader;
 
+import java.io.File;
 import java.util.Map;
 import java.util.WeakHashMap;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -31,10 +34,11 @@ public class RestClient {
     private final IError ERROR;
     private final RequestBody BODY;
     private final LoaderStyle LOADER_STYLE;
+    private final File FILE;
     private final Context CONTEXT;
 
     public RestClient(String url, Map <String, Object> params, IRequest request, ISuccess success
-            , IFailure failure, IError error, RequestBody body,LoaderStyle loaderStyle ,Context context) {
+            , IFailure failure, IError error, RequestBody body, File file, LoaderStyle loaderStyle, Context context) {
         this.URL = url;
         PARAMS.putAll( params );
         this.REQUEST = request;
@@ -43,6 +47,7 @@ public class RestClient {
         this.ERROR = error;
         this.BODY = body;
         this.LOADER_STYLE = loaderStyle;
+        this.FILE = file;
         this.CONTEXT = context;
     }
 
@@ -72,11 +77,22 @@ public class RestClient {
             case POST:
                 call = service.post( URL, PARAMS );
                 break;
+            case POST_RAW:
+                call = service.postRaw( URL, BODY );
+                break;
             case PUT:
                 call = service.put( URL, PARAMS );
                 break;
+            case PUT_RAW:
+                call = service.putRaw( URL, BODY );
+                break;
             case DELETE:
                 call = service.delete( URL, PARAMS );
+                break;
+            case UPLOAD:
+                final RequestBody requestBody = RequestBody.create( MediaType.parse( MultipartBody.FORM.toString()),FILE );
+                final MultipartBody.Part body = MultipartBody.Part.createFormData( "file" ,FILE.getName() ,requestBody);
+                call = RestCreator.getRestService().upload( URL,body );
                 break;
             default:
                 break;
@@ -102,11 +118,27 @@ public class RestClient {
     }
 
     public final void post(){
-        request( HttpMethod.POST );
+
+        if ( BODY == null ) {
+            request( HttpMethod.POST );
+        }else {
+            if ( !PARAMS.isEmpty() ){
+                throw  new RuntimeException( "params must be null!" );
+            }
+            request( HttpMethod.POST_RAW );
+        }
     }
 
     public final void put(){
-        request( HttpMethod.PUT );
+
+        if ( BODY == null ) {
+            request( HttpMethod.PUT );
+        }else {
+            if ( !PARAMS.isEmpty() ){
+                throw  new RuntimeException( "params must be null!" );
+            }
+            request( HttpMethod.PUT_RAW );
+        }
     }
 
     public final void delete(){
--- mavo-core/src/main/java/com/mavolas/mavo/net/RestClientBuilder.java
+++ mavo-core/src/main/java/com/mavolas/mavo/net/RestClientBuilder.java
@@ -8,6 +8,7 @@ import com.mavolas.mavo.net.callback.IRequest;
 import com.mavolas.mavo.net.callback.ISuccess;
 import com.mavolas.mavo.ui.LoaderStyle;
 
+import java.io.File;
 import java.util.Map;
 import java.util.WeakHashMap;
 
@@ -27,8 +28,10 @@ public class RestClientBuilder {
     private  IFailure mFailure = null;
     private  IError mError = null;
     private  RequestBody mBody = null;
+    private File mFile = null;
     private Context mContext = null;
     private LoaderStyle mLoaderStyle = null;
+
 
 
     public RestClientBuilder() {
@@ -49,6 +52,18 @@ public class RestClientBuilder {
     public final RestClientBuilder params(String key,Object value){
 
         this.PARAMS.put( key,value );
+        return this;
+    }
+
+    public final RestClientBuilder file(File file){
+
+        this.mFile = file;
+        return this;
+    }
+
+    public final RestClientBuilder file(String filePath){
+
+        this.mFile = new File( filePath );
         return this;
     }
 
@@ -100,7 +115,7 @@ public class RestClientBuilder {
 
     public final RestClient build(){
 
-        return new RestClient(mUrl, PARAMS, mRequest, mSuccess, mFailure, mError ,mBody ,mLoaderStyle, mContext );
+        return new RestClient(mUrl, PARAMS, mRequest, mSuccess, mFailure, mError ,mBody ,mFile,mLoaderStyle, mContext );
     }
 
 
